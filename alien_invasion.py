5682e2c5ae75d11d8666247953ee02652b48bf95
Scott Bordelon <scott.bordelon@gmail.com>
2022-07-02 18:13:56 -0700

    Start firing bullets from Loki.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """ Overall class to manage game assets and behavior """
@@ -25,19 +26,25 @@ class AlienInvasion:
         pygame.display.set_caption("Posey Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """ Start the main loop for the game. """
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _update_screen(self):
-        # Redraw the screen during each pass through the loop.
+        # Redraw the screen during each pass through the loop. Update images
+        # on the screen, and flip to the new screen.
         self.screen.fill(self.settings.bg_color)
         # Draw the ship at its current location
         self.ship.blitme()
+        # Draw the bullets in the bullets group
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
@@ -62,6 +69,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """ Respond to key releases. """
@@ -71,6 +80,11 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             # Stop moving the ship to the left.
             self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """ Create a new bullet and add it to the bullets group. """
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
         
 #
