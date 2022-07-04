2bd7855d67256a510b65f7a966910fefd1864d14
Scott Bordelon <scott.bordelon@gmail.com>
2022-07-03 17:43:43 -0700

    Make the fleet of Poseys move back and forth, while dropping down when they hit the screen edge.

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,6 +41,14 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         #print(len(self.bullets)) # Make sure bullets get removed from group
+
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge. Then update the positions of all
+        aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
     
     def _create_fleet(self):
         """ Create the fleet of aliens. """
@@ -71,6 +79,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+    
+    def _check_fleet_edges(self):
+        """ Respond appropriately if any aliens have reached an edge. """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """ Drop the entire fleet and change the fleet's direction. """
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
     
     def _update_screen(self):
         # Redraw the screen during each pass through the loop. Update images
@@ -131,6 +152,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
         
