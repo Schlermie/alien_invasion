60863458bdb1b9e73ac6fc479d64072002cc49d6
Scott Bordelon <scott.bordelon@gmail.com>
2022-07-12 21:05:31 -0700

    Display the player score and maintain a displayed high score.

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 class AlienInvasion:
     """ Overall class to manage game assets and behavior """
@@ -30,8 +31,9 @@ class AlienInvasion:
 
         pygame.display.set_caption("Posey Invasion")
 
-        # Create an instance to store game statistics
+        # Create an instance to store game statistics and create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -79,6 +81,11 @@ class AlienInvasion:
         # If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy exisitng bullets and create new fleet.
@@ -182,6 +189,9 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
 
+        # Draw the score information
+        self.sb.show_score()
+
         # Draw the buttons if the game is inactive
         if not self.stats.game_active:
             self.play_button.draw_button()
@@ -231,6 +241,7 @@ class AlienInvasion:
         # Reset the game statistics
         self.stats.reset_stats()
         self.stats.game_active=True
+        self.sb.prep_score()
 
         # Get rid of any remaining aliens and bullets
         self.aliens.empty()
